0a4c1705af4df8bd2d8e694da7e30f7eade87f39
Mukul <amukul82@gmail.com>
2020-04-05 18:01:51 +0530

    add alertmanager

--- flask/app.py
+++ flask/app.py
@@ -1,17 +1,38 @@
-from flask import Flask, Response, jsonify
+from flask import Flask, request, Response, jsonify
 from prometheus_client import multiprocess, Counter, generate_latest, CollectorRegistry, CONTENT_TYPE_LATEST
- 
-app = Flask(__name__)
  
 my_counter = Counter(
     'my_counter', 'A basic counter.',
     ['app_name']
 )
 
+request_count = Counter(
+    'request_count', 'App Request Count',
+    ['app_name', 'method', 'endpoint', 'http_status']
+)
+
+
+def record_request_data(response):
+    request_count.labels(app_name='flask_app', method=request.method, endpoint=request.path, http_status=response.status_code).inc()
+    return response
+
+
+def setup_metrics(app):
+    app.after_request(record_request_data)
+
+app = Flask(__name__)
+setup_metrics(app)
+
 
 @app.route("/")
+@app.route("/hello")
 def hello():
     return "hello"
+
+
+@app.route("/error")
+def error():
+    return "error", 500
 
 
 # Expose metrics.
